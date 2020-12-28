40b879fa67481e7795e626986bb32ec2eea09563
gx1989mao <gx1989mao@gmail.com>
2020-12-28 23:41:14 +0800

    修改myEwavelet增加gamma波段

--- myEwavelet.m
+++ myEwavelet.m
@@ -1,4 +1,4 @@
-function [E_delta,E_theta,E_alpha,E_beta] = myEwavelet(x)
+function [E_delta,E_theta,E_alpha,E_beta,E_gamma] = myEwavelet(x)
 
 order = 6;
 wpt=wpdec(x,order,'db10'); %进行3层小波包分解
@@ -13,7 +13,11 @@ E_wavelet(:) = E_wavelet(nodes_ord(:)); % 顺序调整好
 E_delta = sum(E_wavelet(1:2));
 E_theta = sum(E_wavelet(3:4));
 E_alpha = sum(E_wavelet(5:7));
-E_beta = sum(E_wavelet(8:13));
+E_beta = sum(E_wavelet(8:15));
+E_gamma = sum(E_wavelet(15:24));
 
+figure
+
+% 31.2500   33.2031   35.1563   37.1094   39.0625   41.0156   42.9688   44.9219   46.8750 (24)  48.8281 这里可能有工频干扰了  50.7813   52.7344   54.6875   56.6406   58.5938   60.5469   62.5000
 end
 
